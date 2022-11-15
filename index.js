3b1f0d2005b99700dfa975f64580676eea76f5b1
PaulSantiagoValdivia <116080469+PaulSantiagoValdivia@users.noreply.github.com>
2022-11-15 19:54:00 -0300

    final commit

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ let shoppingCart = [];
 let validate;
 let option;
 let mensaje;
-const airforce1 = 150, cortez = 130, airmax = 180, originals = 130, performance = 120, style = 125, rsfast = 160, runner = 100, authentic = 160, oldSkool = 165, oneStart = 170, dc1 = 120, dc2 = 110, dc3 = 140;
+const airforce1 = 150, cortez = 130, airmax = 180, originals = 130, performance = 120, style = 125, rsfast = 160, runner = 100, authentic = 160, oldSkool = 165, oneStart = 170;
 const productShoes = [
-    { id: 1, name: "nike", type: ["airforce1", "cortez", "airmax"], size: [39, 40, 41, 42, 43], price: [airforce1, cortez, airmax] },
-    { id: 2, name: "adidas", type: ["originals", "performance", "style"], size: [39, 40, 41, 42, 43], price: [originals, performance, style] },
-    { id: 3, name: "puma", type: ["rsfast", "runner"], size: [39, 40, 41, 42, 43], price: [rsfast, runner] },
-    { id: 4, name: "vans", type: ["authentic", "oldSkool", "oneStart"], size: [39, 40, 41, 42, 43], price: [authentic, oldSkool, oneStart] },
+    { id: 1, name: "nike", type: "airforce1", size: [39, 40, 41, 42, 43], price: airforce1 },
+    { id: 2, name: "nike", type: "cortez", size: [39, 40, 41, 42, 43], price: cortez },
+    { id: 3, name: "nike", type: "airmax", size: [39, 40, 41, 42, 43], price: airmax },
+    { id: 4, name: "adidas", type: "originals", size: [39, 40, 41, 42, 43], price: originals },
+    { id: 5, name: "adidas", type: "performance", size: [39, 40, 41, 42, 43], price: performance },
+    { id: 6, name: "adidas", type: "style", size: [39, 40, 41, 42, 43], price: style },
+    { id: 7, name: "puma", type: "rsfast", size: [39, 40, 41, 42, 43], price: rsfast },
+    { id: 8, name: "puma", type: "runner", size: [39, 40, 41, 42, 43], price: runner },
+    { id: 9, name: "vans", type: "authentic", size: [39, 40, 41, 42, 43], price: authentic },
+    { id: 10, name: "vans", type: "oldSkool", size: [39, 40, 41, 42, 43], price: oldSkool },
+    { id: 11, name: "vans", type: "oneStart", size: [39, 40, 41, 42, 43], price: oneStart },
 ];
 
 alert("Bienvenido a shoers valdivia \nPor el momento solo contamos con zapatillas\nPronto tendremos mas novedades para usted");
@@ -26,64 +33,56 @@ function menu() {
     //validamos que el usuario ingrese una opcion que este en el menu 
     do {
         //pedimos al usuario que prosceso desea realizar
-        option = parseInt(prompt("Digite 1 para visualizar nuestro stock de marcas\nDigite 2 buscar una marca\nDigite 3 para visualizar marcas modelos talle y precio "));
+        option = parseInt(prompt("Digite 1 para visualizar nuestro stock\nDigite 2 buscar una marca\nDigite 3 para visualizar marcas modelos talle y precio "));
         //validamos que option coincida con uno de los case sino mostrara un mensaje deafault
         switch (option) {
             //LLamado a funciones 
             case 1: showCatalg(); break;
             case 2: showTrademark(); break;
-            case 3: showAllMark(); break;        
+            case 3: showAllMark(); break;
             default: alert("ingrese una opcion valida");
         }
     } while (option !== 1 && option !== 2 && option !== 3);
 }
 function showCatalg() {
     productShoes.forEach(compra => {
-        alert("contamos con estas marcas :\n" + compra.name);
+        alert("contamos con estos tipos de zpatillas :\n Tipo: " + compra.type + "\nMarca:" + compra.name);
     });
 
 }
 function showTrademark() {
     do {
         nameShoes = prompt("ingrese la marca que desea buscar en la tienda");
-        validate = productShoes.find(product => product.name === nameShoes);
+        validate = productShoes.find((product) => product.name === nameShoes);
         if (validate) {
-            showValidate(validate);
+            showValidate();
         } else {
             alert("la marca ingresada no se encuentra en stock.\n ingrese otra nuevamente");
         }
     } while (!validate);
 }
-function showValidate(validate) {
-    mensaje = ` 
-            tipo: ${validate.type}
-            talla: ${validate.size}`;
-    alert(mensaje);
+function showValidate() {
+    validate = productShoes.filter((product) => product.name === nameShoes);
+    validate.forEach(element => {
+        mensaje = `
+        marca: ${element.name}
+        modelo: ${element.type}
+        precio: ${element.price}U$D 
+        talla: ${element.size}`
+        alert(mensaje)
+    });
+
 
 }
 function showAllMark() {
     let mensaje
-    let typeLength;
-    let iterator = 0;
-    let guardado;
-    nameShoes=prompt("Ingrese el modelo que desea comprar");
     for (const product of productShoes) {
-        typeLength = product.type.length;
-        while (iterator < typeLength) {
-            mensaje = `
+        mensaje = `
         marca: ${product.name}
-        modelo: ${product.type[iterator]}
-        precio: ${product.price[iterator]}U$D 
+        modelo: ${product.type}
+        precio: ${product.price}U$D 
         talla: ${product.size}`;
-            alert(mensaje);
-            iterator++;
-        }
-        
-        iterator = 0;
+        alert(mensaje);
     }
-
 }
-
-
-
 
